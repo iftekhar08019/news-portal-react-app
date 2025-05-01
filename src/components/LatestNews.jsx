0c633bba0205ad4202f73ec56858c426e41b4cc4
Md Iftekharul Alam <68458610+iftekhar08019@users.noreply.github.com>
2025-05-01 11:51:30 +0200

    Navbar and Marquee

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import Marquee from "react-fast-marquee";
 
 const LatestNews = () => {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <>
+      <div className="flex items-center gap-5 bg-base-200 p-3">
+        <p className="text-base-100 bg-secondary px-3 py-2 ">Latest</p>
+        <Marquee className="flex gap-5" pauseOnHover={true} speed={60}>
+        <p className="font-bold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, vel.
+        </p>
+        <p className="font-bold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, vel.
+        </p>
+        <p className="font-bold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, vel.
+        </p>
+        </Marquee>
+      </div>
+    </>
+  );
 };
 
 export default LatestNews;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 function HomeLayout() {
   return (
@@ -11,6 +12,9 @@ function HomeLayout() {
         <section className="w-11/12 mx-auto my-3">
             <LatestNews></LatestNews>
         </section>
+        <nav>
+            <Navbar></Navbar>
+        </nav>
       </header>
       <main></main>
       <section className="left-nav"></section>
